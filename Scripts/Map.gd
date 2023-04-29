9fdd82cef5afdb16764911aa6c62ac3b5ecda32d
HolyBlue <hugobarrandon@gmail.com>
2023-04-29 12:19:51 +0200

    Ajout clic seulement sur cellules productives

--- Scripts/Map.gd
+++ Scripts/Map.gd
@@ -1,8 +1,18 @@
 extends TileMap
 
-var blank : Vector2i = Vector2i(2,0)
+class_name Map
+
+var productiveCellCoord : Vector2i = Vector2i(2,0)
+
+func _ready():
+	var center : Vector2i = Vector2i(get_viewport_rect().size/2) - (tile_set.tile_size/2)
+	var cellPos : Vector2i = local_to_map(center)
+	set_cell(0, cellPos,0, productiveCellCoord)
 
 func _unhandled_input(event):
 	if(event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed()):
 		var cellPos : Vector2i = local_to_map(get_global_mouse_position())
-		set_cell(0, cellPos,0, blank)
+
+func isProductiveCell() -> bool:
+	var textureCoord = get_cell_atlas_coords(0, local_to_map(get_local_mouse_position()))
+	return textureCoord == productiveCellCoord
--- Scripts/main.gd
+++ Scripts/main.gd
@@ -6,6 +6,8 @@ var nbResource : int = 0
 
 @onready
 var UI : InGameUI = $InGame
+@onready
+var _map : Map = $Map
 
 func _ready():
 	UI._startRun()
@@ -13,5 +15,6 @@ func _ready():
 
 func _unhandled_input(event):
 	if(event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed()):
-		nbResource+=1
-		UI.updateResources(nbResource)
+		if(_map.isProductiveCell()):
+			nbResource+=1
+			UI.updateResources(nbResource)
