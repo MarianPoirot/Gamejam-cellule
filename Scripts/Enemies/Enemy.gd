da6c125a65d290a25ed8a857d0f685faa9ab1797
MarianPoirot <81marianp@gmail.com>
2023-04-29 16:52:01 +0200

    Ennemies go to the closest cell

--- Scripts/Enemies/Enemy.gd
+++ Scripts/Enemies/Enemy.gd
@@ -6,8 +6,13 @@ var speed = 0
 var target_position= Vector2(800,450)
 var attack = false
 var move = true
+var closestCell
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	closestCell = get_closest_cell()
+	target_position= closestCell.position
+	
 	var roll = randi()%100
 	if (roll < 99):
 		$AnimatedSprite2D.animation = "virus_move"
@@ -18,7 +23,18 @@ func _ready():
 		$AnimatedSprite2D.scale.x=0.25
 		$AnimatedSprite2D.scale.y=0.25
 
-func _physics_process(delta: float) -> void:
+func get_closest_cell():
+	var min_dist = 99999
+	var min_cell
+	var cells = get_tree().get_nodes_in_group("cells")
+	for cell in cells:
+		var dist = self.position.distance_to(cell.position)
+		if (dist < min_dist):
+			min_dist = dist
+			min_cell = cell
+	return min_cell
+	
+func _physics_process(_delta: float) -> void:
 	if move:
 		var direction = global_position.direction_to(target_position)
 		velocity = direction*speed
@@ -28,7 +44,7 @@ func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
 
 
-func _on_area_2d_input_event(viewport, event, shape_idx):
+func _on_area_2d_input_event(_viewport, event, _shape_idx):
 	if(event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed()):
 		queue_free()
 
