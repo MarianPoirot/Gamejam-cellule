18b800ebf1891406bd06cb8c429d962aa07eeb04
MarianPoirot <81marianp@gmail.com>
2023-04-29 18:23:19 +0200

    Added strength, life and collision

--- Scripts/Enemies/Enemy.gd
+++ Scripts/Enemies/Enemy.gd
@@ -10,6 +10,7 @@ var closestCell
 var strength
 var direction
 var dist
+var life
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -23,11 +24,13 @@ func _ready():
 		$AnimatedSprite2D.scale.x=0.125
 		$AnimatedSprite2D.scale.y=0.125
 		strength = 10
+		life = 1
 	else:
 		$AnimatedSprite2D.animation = "kirbo_move"
 		$AnimatedSprite2D.scale.x=0.25
 		$AnimatedSprite2D.scale.y=0.25
 		strength = 1000
+		life = 2
 
 func get_closest_cell():
 	var min_dist = 9999
@@ -42,15 +45,18 @@ func get_closest_cell():
 	return min_cell
 	
 func _physics_process(_delta: float) -> void:
-	direction = global_position.direction_to(target_position)
-	velocity = direction*speed
-	self.move_and_slide()
+	if move:
+		direction = global_position.direction_to(target_position)
+		velocity = direction*speed
+		self.move_and_slide()
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
 
 func _on_area_2d_input_event(_viewport, event, _shape_idx):
 	if(event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed()):
+		life-=1
+	if life<=0:
 		queue_free()
 
 func _on_cooldown_attack_timeout():
@@ -60,9 +66,10 @@ func _on_cooldown_attack_timeout():
 		target_position= closestCell.position
 	move = true
 	attack=false
+	$Hurtbox/CollisionShape2D.set_deferred("disabled", false)
 
-func _on_click_area_area_entered(area):
+func _on_hurtbox_area_entered(area):
 	move=false
 	attack=true
-	target_position= -target_position
+	$Hurtbox/CollisionShape2D.set_deferred("disabled", true)
 	$CooldownAttack.start()
