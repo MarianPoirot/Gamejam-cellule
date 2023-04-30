9301406dcdc0ac306ec3ad9bca073d46eee76701
MarianPoirot <81marianp@gmail.com>
2023-04-30 09:38:49 +0200

    added function Transform_Prod and transform_attack to cells

--- Scripts/Cellule.gd
+++ Scripts/Cellule.gd
@@ -6,11 +6,18 @@ signal getHit
 
 var life=100
 
+func TransformProd():
+	$AnimatedSprite2D.animation = "Cellule_prod"
+	$ProdTimer.start()
+	
+func TransformAttack():
+	$AnimatedSprite2D.animation = "Cellule_attack"
+	
 func Spawn(dest : Vector2):
+	$AnimatedSprite2D.animation = "Cellule"
 	var tween = get_tree().create_tween()
 	tween.parallel().tween_property(self, "scale", Vector2.ONE*0.1, 1).set_trans(Tween.TRANS_QUAD)
 	tween.parallel().tween_property(self, "position", dest, 1)
-
 
 func _hit(viewport, event, shape_idx):
 	if(event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed()):
@@ -25,3 +32,6 @@ func _on_area_2d_area_entered(area):
 
 func die():
 	queue_free()
+
+func _on_prod_timer_timeout():
+	emit_signal("getHit")
