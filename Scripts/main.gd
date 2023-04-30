c376f3f78da0dc9d5a810a64278d52aef01eeda9
MarianPoirot <81marianp@gmail.com>
2023-04-30 09:08:44 +0200

    restart works

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -19,9 +19,15 @@ var _cellulesPool = $Cellules
 @export var Enemy: PackedScene
 
 func _ready():
+	new_game()
+
+func new_game():
+	$MobTimer.start()
+	_pinTimer.start(rng.randi_range(1,5))
+	
 	UI._startRun()
 	UI.updateResources(nbResource)
-
+	
 	#Cellule originelle
 	var gridCoord = _map.alignCoord(_map.center())
 	var cell = _cellScene.instantiate()
@@ -30,11 +36,6 @@ func _ready():
 	_cellulesPool.add_child(cell)
 	cell.Spawn(gridCoord)
 	
-	new_game()
-
-func new_game():
-	$MobTimer.start()
-	_pinTimer.start(rng.randi_range(1,5))
 
 func _unhandled_input(event):
 	if(event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_RIGHT and event.is_pressed()):
@@ -81,6 +82,9 @@ func _on_start_button_button_down():
 	$MobTimer.stop()
 	_pinTimer.stop()
 	get_tree().call_group("mobs", "queue_free")
+	_map.clear()
+	get_tree().call_group("cells", "queue_free")
+	nbResource = 0
 	new_game()
 
 func newCell(origine : Vector2):
